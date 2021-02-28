7c139e4249cfce19a75d8ac8e3db09224a2a2def
xeavier <chandarmtsb007@gmail.com>
2021-02-28 10:37:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-1fehtx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-1fehtx)
---
+++ index.js
@@ -0,0 +1,13 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const departments = [{
+  "id":1,
+  "country_code":"in",
+  "name":"Electronics",
+  "description":"",
+}]
