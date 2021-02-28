c1c1c2e581d2df4c96c6310c57f0490d20ed8daa
xeavier <chandarmtsb007@gmail.com>
2021-02-28 10:52:15 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+const appDiv = document.getElementById("app");
+//appDiv.innerHTML = `<h1>${departments}</h1>`;
 
-const departments = [{
-  "id":1,
-  "country_code":"in",
-  "name":"Electronics",
-  "description":"",
-}]
+const departments = [
+  {
+    id: 1,
+    country_code: "in",
+    name: "Tv,Appliances,Electronics",
+    description: "",
+    isAvalible: true
+  },
+  {
+    id: 2,
+    country_code: "in",
+    name: "Mobiles,Computers",
+    description: "",
+    isAvalible: true
+  },
+  {},
+  {},
+  {},
+  {},
+  {}
+];
+
+appDiv.innerHTML = `<h1>${JSON.stringify(departments)}</h1>`;
